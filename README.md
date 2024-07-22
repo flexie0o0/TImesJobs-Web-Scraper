d397bd5318dd2ff583b7ce7a4f2256667b4bd350
flexie0o0 <wabipie0o0@gmail.com>
2024-07-23 00:26:10 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# TImesJobs-Web-Scraper" 
---
+++ main.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+print("Type some skills you are not familiar with:")
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=cybersecurity&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+    for index, job in enumerate (jobs):
+        date_posted = job.find('span', class_='sim-posted').span.text
+        if 'few' in date_posted:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
+
+            if unfamiliar_skill not in skills:
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f"Company Name: {company_name.strip()} \n")
+                    f.write(f"Required Skills: {skills.strip()} \n")
+                    f.write(f"Date posted: {date_posted.strip()} \n")
+                    f.write(f"More Info: {more_info} \n")
+                print(f'File saved: {index}')
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60)
